d81211e87a88eaf4b2a465b0e83f6b182c69cf5c
DRobbie39 <phangiadat123@gmail.com>
2025-02-14 16:03:56 +0700

    edit the shortened display of the video's posting date and time

--- Entertainment_Web_API/Entertainment_Web_API/obj/Debug/net8.0/Entertainment_Web_API.AssemblyInfo.cs
+++ Entertainment_Web_API/Entertainment_Web_API/obj/Debug/net8.0/Entertainment_Web_API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entertainment_Web_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d49eb71557dd7c45b58b5db4c7541dada601c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b6a6b05b611556969260f968c625dfe4479bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entertainment_Web_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entertainment_Web_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
